fdf6c783cec022171c72b9cdae2f1fd1e0282c73
naty55 <natymm314@gmail.com>
2021-05-28 17:58:01 +0300

    add function get words

--- dict_api.py
+++ dict_api.py
@@ -1,10 +1,19 @@
 import csv
 
+
 class DictAPI:
 
-    def __init__(self, dictfile):
-        self.dictfile = csv.reader(open(dictfile, encoding='utf-8'))
+    """
+    DictApi lets you control the dictionary (csv file) with more ease
+    """
 
+    def __init__(self, dictfile):
+        self.csvfile = open(dictfile, 'r', encoding='utf-8', newline='')
+        self.reader = csv.reader(self.csvfile, delimiter=',')
+        self.rows = list(self.reader)
 
     def get_words(self, start_index, amount):
-        pass
+        return self.rows[start_index:start_index + amount]
+
+    def close(self):
+        self.csvfile.close()
