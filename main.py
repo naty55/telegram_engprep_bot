44ec0cd9d4df4a78684e43bd2b8a33e8476c02c1
naty55 <natymm314@gmail.com>
2021-07-22 01:09:00 +0300

    some refactoring; added new message handler

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 from bot_functions import (start_handler,
                            button_map_handler,
-                           conversation_map_handler,
                            register_handler,
                            quiz_me_handler,
                            quiz_handler,
-                           menu_handler)
+                           menu_handler,
+                           age_handler)
 
-from telegram.ext import CommandHandler, Updater, CallbackQueryHandler, ConversationHandler, PollAnswerHandler
+from telegram.ext import CommandHandler, Updater, CallbackQueryHandler, PollAnswerHandler, MessageHandler, Filters
+
 
 with open("keys.txt", 'r') as f:
     token = f.readline()
 
 updater = Updater(token=token)
 bot = updater.bot
-
 dispatcher = updater.dispatcher
+
 dispatcher.add_handler(CommandHandler('start', start_handler))
 dispatcher.add_handler(CommandHandler('register', register_handler))
 dispatcher.add_handler(CommandHandler('menu', menu_handler))
 dispatcher.add_handler(CommandHandler('quiz_me', quiz_me_handler))
 dispatcher.add_handler(CallbackQueryHandler(button_map_handler))
 dispatcher.add_handler(PollAnswerHandler(quiz_handler, pass_user_data=True, pass_chat_data=True))
-# dispatcher.add_handler(ConversationHandler(conversation_map_handler))
+dispatcher.add_handler(MessageHandler(Filters.regex("^[1-9][0 -9]*$"), age_handler))
+
 updater.start_polling()
 updater.idle()
