5fc1f0eff7f7c0f33011cc34111b9ea689686372
naty55 <natymm314@gmail.com>
2021-09-02 01:42:42 +0300

    added new logger; added sessions to apis (more than one file needs access to it

--- apis.py
+++ apis.py
@@ -1,5 +1,6 @@
 from DatabaseAPI import DataBase
 from DictAPI import DictAPI
+from SessionsDict import SessionsDict
 import logging
 ######
 # This file is for any object that should be
@@ -9,14 +10,24 @@ db_api = DataBase('test.db')
 dict_api = DictAPI('new_dict.csv')
 
 # Logging shit goes here
-# --- This is not well written - to be fixed ---
+# --- This is not well written - to be fixed in the future ---
 format_string = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 formatter = logging.Formatter(format_string)
 logging.basicConfig(format=format_string, level=logging.INFO)
 bot_logger = logging.getLogger('bot_logger')
-file_handler = logging.FileHandler('bot_log.log')
-file_handler.setFormatter(formatter)
-bot_logger.addHandler(file_handler)
+bot_file_handler = logging.FileHandler('bot_log.log')
+bot_file_handler.setFormatter(formatter)
+bot_logger.addHandler(bot_file_handler)
+
+# Sessions object
+sessions = SessionsDict(lambda: False)
+
+# Message Logger
+message_logger = logging.getLogger("MessageLogger")
+message_logger.propagate = False
+message_file_handler = logging.FileHandler('messages.log')
+message_file_handler.setFormatter(formatter)
+message_logger.addHandler(message_file_handler)
 
 
 
