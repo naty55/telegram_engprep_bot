0e765de6f3110a6e1b02e9054f03c76ca705327c
naty55 <natymm314@gmail.com>
2021-09-02 01:52:40 +0300

    added option to notify all users when starting the bot

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from bot_functions import (start_handler,
                            contact_handler,
                            notify_all_users
                            )
-
+import sys
 from telegram.ext import CommandHandler, Updater, CallbackQueryHandler, PollAnswerHandler, MessageHandler, Filters
 
 with open("keys.txt", 'r') as f:
@@ -29,6 +29,10 @@ dispatcher.add_handler(CommandHandler('quiz_me_he', quiz_me_he_handler))
 dispatcher.add_handler(CallbackQueryHandler(button_map_handler))
 dispatcher.add_handler(PollAnswerHandler(quiz_handler, pass_user_data=True, pass_chat_data=True))
 dispatcher.add_handler(MessageHandler(Filters.regex("^[1-9][0 -9]*$"), age_handler))
+
+if len(sys.argv) > 1:
+    notify_all_users(sys.argv[1], updater.bot)
+
 
 updater.start_polling()
 updater.idle()
