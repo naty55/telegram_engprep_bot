0c526886cd5460a59663f065f5bc1f585fac9373
naty55 <natymm314@gmail.com>
2021-09-19 02:00:16 +0300

    refactoring

--- SessionsDict.py
+++ SessionsDict.py
@@ -7,7 +7,7 @@ class SessionsDict(defaultdict):
     every time a person is being pulled out it will update its time
     """
     def get(self, key):
-        person = super(SessionsDict, self).get(key)
+        person = super(SessionsDict, self).get(int(key))
         if person:
             person.touch()
         return person
