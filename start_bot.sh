5d0e9a0011884b4d19f5a4b5277ffb64ac7bdef8
naty55 <natymm314@gmail.com>
2021-09-30 22:25:42 +0300

    Added bash script file to start bot automatically

--- start_bot.sh
+++ start_bot.sh
@@ -1,7 +1,9 @@
 #!bin/bash
-echo "starting bot!"
+echo "[INFO] change directory to telegram_engprep_bot"
 cd telegram_engprep_bot
+echo "[INFO] pulling latest updates from github.com"
 git pull
+echo "[INFO] starting the bot"
 python main.py
 
 
